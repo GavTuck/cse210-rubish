4603eac001890229454f00f5c0ecca7240e88cbd
GavTuck <tuc21007@byui.edu>
2024-04-25 18:42:52 -0600

    finsihed work

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?")
+        string fname = Consule.ReadLine();
+        Console.WriteLine("What is your last name?")
+        string lname = Consule.ReadLine();
+
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}")
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your grade percentage?")
+        
+        string grade = Conole.ReadLine();
+        int num = int.Parse(grade);
+        if (grade >= 90)
+
+        string letter ="";
+        {
+            string letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            string letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            string letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            string letter = "D";
+        }
+        else
+        {
+            string letter = "F";
+        }
+        
+        Consule.WriteLine($"Your grade in the class was a {letter}")
+        
+        if (grade >= 70)
+        {
+            Consule.WriteLine("You passed");
+        }
+        else
+        {
+            Consule.WriteLine("You failed, you suck");
+        }
     }
 }
