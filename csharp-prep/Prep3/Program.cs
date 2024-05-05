462ac13f5b2ac31e7ff73477d027a224adb53e28
GavTuck <tuc21007@byui.edu>
2024-05-04 23:22:33 -0600

    changes of the week

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random()
+        int number = randomGenerator.Next(1,100)
+        
+        
+       
+        
+        while (response != number)
+        {
+            Console.WriteLine("What is your guess");
+            response = int.Parse(Consule.ReadLine())
+        }
+        if (response >= number)
+        {
+            Consule.WriteLine("Higher")
+        }
+        else if (number >= response)
+        {
+            Consule.WriteLine("Lower")
+        }
+
+    
+    
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,52 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+
+
+        static void Main(){
+             DisplayWelcome();
+            
+            string intro = PromptUserName();
+
+            int user_number = PromptUserNumber();
+
+            int sqr_user_number = SquareNumber(int user_number);
+
+            DisplayResult(string intro ,int sqr_user_number);
+
+
+
+
+
+        }
+        
+        static void DisplayWelcome(){
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static void PromptUserName(){
+            Consule.WriteLine("Please enter your name:");
+            string name = Read.Consule();
+
+            return name
+        }
+
+        static void PromptUserNumber(){
+            Consule.WriteLine("Please enter your favorite number:");
+            int number = Read.Consule();
+
+            return number
+        }
+
+        static void SquareNumber(int number){
+            int sqr = number * number;
+            return sqr;
+        }
+        static void DisplayResult(string name, int sqr){
+            Consule.WriteLine($"{name}, the square of your number is {sqr} ")
+
+        }
+        }
+
     }
 }
