22a78658a8431f4679a47b614f1b0c9f6f515c78
GavTuck <tuc21007@byui.edu>
2024-05-10 08:49:00 -0600

    changes of the week

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,70 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Resume resume1 = new Resume();
+        resume1._jobs = new List<Job>();
+       
+
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+       
+        resume1.Display();
+
     }
+    
 }
+
+        class Job
+        {
+           public string _company;
+
+           public string _jobTitle;
+
+           public int _startYear;
+
+           public int _endYear;
+
+           public void Display(){
+            Console.WriteLine($"{_jobTitle}({_company}) {_startYear}-{_endYear}");
+
+
+           }
+
+
+
+        }
+
+         class Resume
+        {
+            public string _name;
+
+            public List<Job> _jobs;
+
+            public void Display(){
+                Console.WriteLine($"Name: {_name}");
+
+                foreach (Job j in _jobs){
+
+                    j.Display();
+                }
+            }
+
+
+        }
+
+        
---
+++ sandbox/Sandbox/Course.cs
@@ -0,0 +1,23 @@
+
+
+    class Course
+
+    {
+        static void Main(string[] args)
+        {
+            public string _lassCode;
+
+            public string className;
+
+            public int credits;
+
+            public string color;
+
+            //Method
+
+            public void Display(){
+                Console.WriteLine($"{-classCode} {_className} {_credits} {_color}")
+                }
+        }
+
+     }
