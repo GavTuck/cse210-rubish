a77634ca5612b319495b315e7c1d145305581b5b
GavTuck <tuc21007@byui.edu>
2024-05-18 20:46:55 -0600

    donnnnnnne

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,22 +1,21 @@
+using System;
+using System.IO;
 class Journal{
 
-List<string>  = new List<string>()
 
 
 
 
-    public void LoadJournal{
+    public void LoadJournal()
+    {
         
-        
-        string filename = "myFile.txt";
+        Console.WriteLine("Please enter filename ");
+        string filename = Console.ReadLine();
         string[] lines = System.IO.File.ReadAllLines(filename);
 
         foreach (string line in lines)
         {
-            string[] parts = line.Split(",");
-
-            string prompt = parts[0];
-            string response = parts[1];
+            Console.WriteLine(line);
         }  
 
 
@@ -24,7 +23,24 @@ List<string>  = new List<string>()
 
 
     } 
+
+    public void SaveFile(List<Entry> NewEntries){
+        Console.WriteLine("Enter a filename: ");
+        string filename = Console.ReadLine();
+        using(StreamWriter outputFile = new StreamWriter(filename, true)){
+            foreach(Entry NewEntry in NewEntries)
+                outputFile.WriteLine(NewEntry.Display(NewEntry));
+        }
+        NewEntries.Clear();
+
+
+    }
+
+    public void DisplayAllEntries(List<Entry> NewEntries){
+
+        foreach(Entry NewEntry in NewEntries)
+                Console.WriteLine(NewEntry.Display(NewEntry));
+    }
 }
 
 
-}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,10 +1,47 @@
 using System;
+using System.IO;
+using System.Numerics;
 
 class Program
 {
+    static public Journal work_plz = new Journal();
     static void Main(string[] args)
     {
+        List<Entry> NewEntries = new List<Entry>();
+        string choice = "0";
+        Console.WriteLine("Lets gooooooo!!!");
+        while (choice != "5")
+        {
+            Console.WriteLine("Please select one of the following choices: \n 1. Write \n 2. Display \n 3. Load \n 4. Save \n 5.Quit");
+            Console.WriteLine("What would you like to do?");
+            choice = Console.ReadLine();
+
+            if (choice == "1"){
+                Entry NewEntry = new Entry();
+                NewEntry = NewEntry.CreateEntry();
+                NewEntries.Add(NewEntry);
+            }
+            else if (choice == "2"){
+                work_plz.DisplayAllEntries(NewEntries);
+            }
+            else if(choice == "3"){
+                work_plz.LoadJournal();
+            }
+            else if(choice == "4"){
+                work_plz.SaveFile(NewEntries);
+            }
+            else if(choice == "5"){
+                Console.WriteLine("Goodbye");
+            }
+            else{
+                Console.WriteLine("Ops, try again");
+            }
+        }
+
         
-        Console.WriteLine("Hello Develop02 World!");
+        
+
+        
+        // Console.WriteLine("Hello Develop02 World!");
     }
 }
